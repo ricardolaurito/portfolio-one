bb78e8ee10719e057434ddc1977dad4076e4cd6b
Ricardo Laurito <ricardolaurito@yahoo.com>
2023-07-25 01:12:17 -0500

    Ajustado hover do menu e herou

--- assets/js/validate.js
+++ assets/js/validate.js
@@ -1,47 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var tl = gsap.timeline();
+    const tl = gsap.timeline();
 
     tl
-    // .from(".menu__1", {
-    //     duration: 1,
-    //     y: 50,
-    //     opacity: 0,
-    //     delay: 0.5,
-    //     ease: "power3.out",
-    // })
-    // .from(".menu__2", {
-    //     duration: 1,
-    //     y: 50,
-    //     opacity: 0,
-    //     ease: "power3.out",                        
-    // }, "-=0.8")
-    // .from(".menu__3", {
-    //     duration: 1,
-    //     y: 50,
-    //     opacity: 0,
-    //     ease: "power3.out",               
-    // }, "-=0.8")
-    // .from(".menu__4", {
-    //     duration: 1,
-    //     y: 50,
-    //     opacity: 0,
-    //     ease: "power3.out",               
-    // }, "-=0.8")
-    // .from(".menu__5", {
-    //     duration: 1,
-    //     y: 50,
-    //     opacity: 0,
-    //     ease: "power3.out",             
-    // }, "-=0.8")
-    // .from(".menu__6", {
-    //     duration: 1,
-    //     y: 50,
-    //     opacity: 0,
-    //     ease: "power3.out",             
-    // }, "-=0.8")
+    .from(".menu__1", {
+        duration: 1,
+        y: 50,
+        opacity: 0,
+        delay: 0.8,
+        ease: "power3.out",
+    })        
+    .from(".menu__right", {
+        duration: 1,
+        opacity: 0,
+        y: 50,        
+        ease: "power3.out",                        
+    }, "-=0.8")    
     .from(".container__item div", {
         duration: 1,
-        y: 100,
+        y: 80,
         opacity: 0,
         delay: 1.5,
         stagger: {each: 0.2},
@@ -49,14 +25,14 @@ document.addEventListener('DOMContentLoaded', () => {
     }, "-=0.8")
     .from(".bg__hero", {
         duration: 1,
-        y: 100,
+        y: 0,
         opacity: 0,
         stagger: {each: 0.2},
         ease: "power3.out",             
     }, "-=0.8")
     .from(".container__item img", {
         duration: 1,
-        y: 100,
+        y: 80,
         opacity: 0,
         stagger: {each: 0.2},
         ease: "power3.out",             
